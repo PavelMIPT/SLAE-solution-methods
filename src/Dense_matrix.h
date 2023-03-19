f5700b7f0a66bfde5b77c783d92f72fd880c1228
PavelMIPT <Pavelznaka@gmail>
2023-03-19 17:48:41 +0300

    Dense_Matrix

--- src/Dense_matrix.h
+++ src/Dense_matrix.h
@@ -3,33 +3,39 @@
 #pragma once
 
 class Dense_Matrix{   
-    public:
+    private:
         // размер матрицы, m - размер строки, n - размер столбца
         unsigned int m, n;
         std::vector<double> A;
+
+    public:
         Dense_Matrix(std::vector<double>  A0, unsigned int x, unsigned int y): A(A0), m(x), n(y){}
         Dense_Matrix(const Dense_Matrix& c) = default;
 
         // метод умножения матрицы(m, n) на вектор - столбец (m)
-        std::vector<double> Multiplicate_column(const std::vector<double> & a);
+        std::vector<double> Multiplicate_column(const std::vector<double> & a) const;
 
         // метод умножения матрицы(m, n) на вектор - строку (n)
-        std::vector<double> Multiplicate_line (const std::vector<double> & a);
+        std::vector<double> Multiplicate_line (const std::vector<double> & a) const;
 
         // вернуть столбец матрицы(m, n) с номером i
-        std::vector<double> Get_column (size_t i);
+        std::vector<double> Get_column (size_t i) const;
 
-        //оператор умножения матрицы(n, n) на матрицу(n. n) за (o(n^3))- можно лучше ?
-        Dense_Matrix operator * (const Dense_Matrix & a);
+        //оператор умножения матрицы(n, n) на матрицу(n. n) за (o(n^3))
+        Dense_Matrix operator * (const Dense_Matrix & a) const;
 
         //оператор вычитания матрицы(n, n) из матрицы (n. n) 
-        Dense_Matrix operator - (const Dense_Matrix & a);
+        Dense_Matrix operator - (const Dense_Matrix & a) const;
 
         //оператор умножения матрицы(n. n) на число
-        Dense_Matrix operator * (double k);
+        Dense_Matrix operator * (double k) const;
+
+        // доступ к элементу
+        double get_element (size_t i, size_t j) const;
 
 };
-std::vector<double> Dense_Matrix :: Multiplicate_column(const std::vector<double> & a){
+std::vector<double> Dense_Matrix :: Multiplicate_column(const std::vector<double> & a) const
+{
     std::vector<double> y (this->n, 0);
     for (size_t j = 0; j < this->A.size()/a.size(); ++j) {
         double tmp = 0;
@@ -42,7 +48,8 @@ std::vector<double> Dense_Matrix :: Multiplicate_column(const std::vector<double
     return y;
 }
 
-std::vector<double> Dense_Matrix :: Multiplicate_line (const std::vector<double> &a){
+std::vector<double> Dense_Matrix :: Multiplicate_line (const std::vector<double> &a) const
+{
     std::vector<double> b(this->A.size() * a.size());
     for (size_t i = 0; i < a.size(); ++i){
         for(size_t j = 0; j < a.size(); ++j){
@@ -53,7 +60,8 @@ std::vector<double> Dense_Matrix :: Multiplicate_line (const std::vector<double>
 
 }
 
-std::vector<double> Dense_Matrix :: Get_column (size_t i){
+std::vector<double> Dense_Matrix :: Get_column (size_t i) const
+{
     std::vector<double> v(this->n);
     for(size_t j = i * this->n; j < i * this->n + this->n; ++j){
         v[j - i * this->n] = this->A[j];
@@ -63,7 +71,8 @@ std::vector<double> Dense_Matrix :: Get_column (size_t i){
 
 
 // умножаем на матрицу a
-Dense_Matrix Dense_Matrix :: operator * (const Dense_Matrix & a){
+Dense_Matrix Dense_Matrix :: operator * (const Dense_Matrix & a) const
+{
     std::vector <double> res(a.A.size());
     for (size_t i = 0; i < a.n; ++i){
         for (size_t j = 0; j < this->n; ++j){
@@ -78,7 +87,8 @@ Dense_Matrix Dense_Matrix :: operator * (const Dense_Matrix & a){
 }
 
 //вычитаем матрицу a из исходной матрицы такого же размера
-Dense_Matrix Dense_Matrix :: operator - (const Dense_Matrix & a){
+Dense_Matrix Dense_Matrix :: operator - (const Dense_Matrix & a) const
+{
     std::vector <double> res(a.A.size());
     for (size_t i = 0; i < a.n; ++i){
         for (size_t j = 0; j < a.n; ++j){
@@ -89,11 +99,15 @@ Dense_Matrix Dense_Matrix :: operator - (const Dense_Matrix & a){
     return Res;
 }
 
-Dense_Matrix Dense_Matrix :: operator * (double k){
+Dense_Matrix Dense_Matrix :: operator * (double k) const
+{
     std::vector <double> res(this->A.size());
     for (size_t i = 0; i < this->A.size(); ++i){
         res[i] = this->A[i] * k;
     }
-    Dense_Matrix Res (res, this->n, this->n);
-    return Res;
+}
+
+double Dense_Matrix :: get_element(size_t i, size_t j) const
+{
+    return this->A[this->m * i + j];
 }
